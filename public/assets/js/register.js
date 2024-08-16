266a7ffe1a07d0f01ed7b185583ea31b581092f2
clmjunior <claudiomartinsjr6@gmail.com>
2024-08-16 10:21:40 -0300

    fix

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -1,22 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    window.addEventListener('scroll', function() {
-        var header = document.querySelector('.navbar');
-        var navBot = document.querySelector('.nav-bot');
-        var icons = document.querySelectorAll('.icon-img');
-        var iconImg = document.querySelectorAll('.categ-icon');
-        var navLogo = document.querySelector('.nav-logo');
-        var searchbar = document.querySelector('.search-input');
-
-        if (window.scrollY < 60) {
-            header.style.padding = '12px 80px 12px 80px';
-            navBot.style.height = '30px';
-            navLogo.style.width = '200px';
-            searchbar.style.width = '350px';
-            header.style.boxShadow = '1px 1px 10px #eee';
-            icons.forEach(icon => icon.style.display = 'none');
-            iconImg.forEach(icon => icon.style.display = 'none');
-        }
-    });
 
     document.getElementById('document').addEventListener('input', function() {
         let documentValue = this.value;
